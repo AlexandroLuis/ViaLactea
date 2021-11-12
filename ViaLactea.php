d4be268083a66a3c6100ce6be11284a0aa283ba8
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-11-12 18:06:24 -0300

    Update ViaLactea.php

--- ViaLactea.php
+++ ViaLactea.php
@@ -1,4 +1,4 @@
-<?php  
+<?php 
 	$hostname='sql10.freemysqlhosting.net';	//Nome do Servidor
 	$username='sql10450701';		//Nome do Usuario do servidor
 	$pass='7hc9sSem9m';				//Senha do servidor
@@ -8,7 +8,15 @@
 	$db=Mysqli_Connect($hostname, $username, $pass);	 
 	Mysqli_Select_db($db, $bd);
 	
-	$ip_address = gethostbyname("www.whatismyipaddress.com");  
+	if (!empty($_SERVER["HTTP_CLIENT_IP"]))
+		$ip = $_SERVER["HTTP_CLIENT_IP"];
+	elseif (!empty($_SERVER["HTTP_X_FORWARDED_FOR"]))
+		$ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
+	else
+		$ip = $_SERVER["REMOTE_ADDR"];
+	
+	$ip_address = $ip;
+	
 	echo "your ip address is - ".$ip_address;   
 	
 	$sqlinsert ="insert into info values ('','$ip_address')";
