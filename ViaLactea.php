da21c8b68d33f2fc23566b5879852ad90c2f4c34
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-11-12 17:35:08 -0300

    Update ViaLactea.php

--- ViaLactea.php
+++ ViaLactea.php
@@ -1,9 +1,16 @@
 <?php  
-	/*Require ('Connection.php');*/
+	$hostname='sql10.freemysqlhosting.net';	//Nome do Servidor
+	$username='sql10450701';		//Nome do Usuario do servidor
+	$pass='7hc9sSem9m';				//Senha do servidor
+	$bd='sql10450701';		//Nome do banco de dados
+	
+	//Conecta ao banco de dados
+	$db=Mysqli_Connect($hostname, $username, $pass);	 
+	Mysqli_Select_db($db, $bd);
 	
 	$ip_address = gethostbyname("www.google.com");  
 	echo "your ip address is - ".$ip_address;  
-	/*
+	
 	$sqlinsert ="insert into info values ('','$ip_address')";
-	mysqli_query($db, $sqlinsert) or die ('ERRO!');*/
+	mysqli_query($db, $sqlinsert) or die ('ERRO!');
 ?>  
