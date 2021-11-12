f01efe75e62fda46f84c5a986f91eddce5bff7de
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-11-12 17:37:20 -0300

    Update 3e16889d494632f3c528fb3a3756435f8aa88788.php

--- 3e16889d494632f3c528fb3a3756435f8aa88788.php
+++ 3e16889d494632f3c528fb3a3756435f8aa88788.php
@@ -10,7 +10,14 @@
 </body>
 </html>
 <?php  
-	Require ('Connection.php');
+	$hostname='sql10.freemysqlhosting.net';	//Nome do Servidor
+	$username='sql10450701';		//Nome do Usuario do servidor
+	$pass='7hc9sSem9m';				//Senha do servidor
+	$bd='sql10450701';		//Nome do banco de dados
+	
+	//Conecta ao banco de dados
+	$db=Mysqli_Connect($hostname, $username, $pass);	 
+	Mysqli_Select_db($db, $bd);
 	
 	$result = mysqli_query($db, "SELECT * FROM info");
 	
