842095899698ba600b57df8bef8d1fb5c9d113ad
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-11-12 19:14:48 -0300

    Add files via upload

--- 3e16889d494632f3c528fb3a3756435f8aa88788.php
+++ 3e16889d494632f3c528fb3a3756435f8aa88788.php
@@ -1,31 +1,34 @@
 <html>
 </body>
-<table>
-	<tr>
-		<td width="30px">ID      </td>
-		<td width="100px">IP</td>
-	<tr>
-</table>
-<hr>
+	<table>
+		<tr>
+			<td width="75px">ID</td>
+			<td width="130px">IP</td>
+			<td width="300px">DATE</td>
+			<td width="130px">BROWSER</td>
+			<td width="130px">OP SISTEM</td>
+		<tr>
+	<?php  
+		$hostname='sql10.freemysqlhosting.net';	//Nome do Servidor
+		$username='sql10450701';		//Nome do Usuario do servidor
+		$pass='7hc9sSem9m';				//Senha do servidor
+		$bd='sql10450701';		//Nome do banco de dados
+		
+		//Conecta ao banco de dados
+		$db=Mysqli_Connect($hostname, $username, $pass);	 
+		Mysqli_Select_db($db, $bd);
+		
+		$result = mysqli_query($db, "SELECT * FROM info order by id desc");
+		
+		while($row = mysqli_fetch_assoc($result)){
+			echo"<tr>
+					<td>" .$row['id'] ."</td>
+					<td>" ."<a href=https://whatismyipaddress.com/ip/".$row['ip_address'] .">".$row['ip_address'] ."</a></td>
+					<td>" .$row['time'] ."</td>
+					<td>" .$row['browserr'] ."</td>
+					<td>" .$row['ops'] ."</td>";
+		}
+	?> 
+	</table>
 </body>
 </html>
-<?php  
-	$hostname='sql10.freemysqlhosting.net';	//Nome do Servidor
-	$username='sql10450701';		//Nome do Usuario do servidor
-	$pass='7hc9sSem9m';				//Senha do servidor
-	$bd='sql10450701';		//Nome do banco de dados
-	
-	//Conecta ao banco de dados
-	$db=Mysqli_Connect($hostname, $username, $pass);	 
-	Mysqli_Select_db($db, $bd);
-	
-	$result = mysqli_query($db, "SELECT * FROM info");
-	
-	while($row = mysqli_fetch_assoc($result)){
-		echo"<tr>
-				<td>" .$row['id'] ."   -   </td>
-				<td>" ."<a href=https://whatismyipaddress.com/ip/".$row['ip_address'] .">".$row['ip_address'] ."</a></td>
-			</tr>";
-		?><br><?php
-	}
-?> 
